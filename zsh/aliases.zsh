6d51e45063f7eafcaab2e9457b369881c7d0f257
Thomas von Deyen <thomas@vondeyen.com>
2017-01-16 12:16:59 +0100

    Rename my got to code alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-alias gtcf='cd ~/code'
+alias code='cd ~/code'
 alias gtaf='cd ~/code/ruby/gems/alchemy_cms'
 alias test_alchemy='rm Gemfile.lock; bundle install; bundle exec rake alchemy:spec:prepare; bundle exec rspec'
