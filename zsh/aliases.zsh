204134f03f33d1ae0da443ea15c7ea14d251dbd7
Thomas von Deyen <thomas@vondeyen.com>
2017-02-20 21:07:47 +0100

    Always have -R on less command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,4 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias code='cd ~/code'
 alias gtaf='cd ~/code/alchemy_cms'
 alias test_alchemy='rm Gemfile.lock; bundle install; bundle exec rake alchemy:spec:prepare; bundle exec rspec'
+alias less='less -R'
