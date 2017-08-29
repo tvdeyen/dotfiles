0ed3137adb80d3b3a15f9e8c3d269f5247ccfb7b
Thomas von Deyen <thomas@vondeyen.com>
2017-08-29 21:19:32 +0200

    Add more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,6 @@ alias code='cd ~/code'
 alias gtaf='cd ~/code/alchemy_cms'
 alias test_alchemy='rm Gemfile.lock; bundle install; bundle exec rake alchemy:spec:prepare; bundle exec rspec'
 alias less='less -R'
+alias git-clean-up="git branch --merged | grep -v -e 'master' -e '\*' | xargs -n 1 git branch -d && git remote prune origin"
+alias candles='cd ~/code/candlescience/CSpree'
+alias beer='bundle exec rspec'
