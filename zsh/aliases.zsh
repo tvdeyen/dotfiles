c029254efc77401f1937201a634188d378fdfe29
Thomas von Deyen <thomas@vondeyen.com>
2018-11-22 13:14:59 +0100

    Add brandeins alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias gtcf='cd ~/code'
 alias gtaf='cd ~/code/alchemy_cms'
+alias gtb1='cd ~/code/brandeins/kiosk'
 alias test_alchemy='rm Gemfile.lock; bundle install; bundle exec rake alchemy:spec:prepare; bundle exec rspec'
 alias less='less -R'
 alias git-clean-up="git branch --merged | grep -v -e 'master' -e '\*' | xargs -n 1 git branch -d && git remote prune origin"
