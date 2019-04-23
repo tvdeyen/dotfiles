a43e14bfdea16e64ef05bb465e04b98696083ce2
Thomas von Deyen <thomas@vondeyen.com>
2019-04-23 09:52:12 +0200

    Add sqlite env

--- mysql/env.zsh
+++ mysql/env.zsh
@@ -1,3 +1,3 @@
-export LDFLAGS="-L/usr/local/opt/mysql@5.7/lib"
-export CPPFLAGS="-I/usr/local/opt/mysql@5.7/include"
-export PKG_CONFIG_PATH="/usr/local/opt/mysql@5.7/lib/pkgconfig"
+export LDFLAGS="-L/usr/local/opt/mysql@5.7/lib $LDFLAGS"
+export CPPFLAGS="-I/usr/local/opt/mysql@5.7/include $CPPFLAGS"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/mysql@5.7/lib/pkgconfig"
---
+++ sqlite/env.zsh
@@ -0,0 +1,2 @@
+export LDFLAGS="-L/usr/local/opt/sqlite/lib $LDFLAGS"
+export CPPFLAGS="-I/usr/local/opt/sqlite/include $CPPFLAGS"
