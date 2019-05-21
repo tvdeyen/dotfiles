7e0e7fa2cbadd00709b70a4b2b522314999bf9bb
Thomas von Deyen <thomas@vondeyen.com>
2019-05-21 17:35:46 +0200

    Fix Heroku CLI autocompletion

--- heroku/completion.zsh
+++ heroku/completion.zsh
@@ -1,1 +1,2 @@
-heroku autocomplete:script zsh
+# heroku autocomplete setup
+HEROKU_AC_ZSH_SETUP_PATH=/home/tvd/.cache/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
