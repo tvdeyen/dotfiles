20dbb5027361ea9c81ba7d4955f5f8538e5468d3
Thomas von Deyen <thomas@vondeyen.com>
2022-04-04 21:06:45 +0200

    Remove libpg bin path from path

--- postgresql/path.zsh
+++ postgresql/path.zsh
@@ -2,4 +2,3 @@ export PATH="/Applications/Postgres.app/Contents/Versions/12/bin:$PATH"
 # Installed libpq via homebrew
 export LDFLAGS="-L/opt/homebrew/opt/libpq/lib $LDFLAGS"
 export CPPFLAGS="-I/opt/homebrew/opt/libpq/include $CPPFLAGS"
-export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
