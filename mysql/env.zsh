8205f061db73dbafb6b4a13ff426e212f422b2d5
Thomas von Deyen <thomas@vondeyen.com>
2022-04-04 21:06:04 +0200

    Fix mysql build flags

--- mysql/env.zsh
+++ mysql/env.zsh
@@ -1,3 +1,3 @@
-# export LDFLAGS="-L/usr/local/opt/mysql/lib $LDFLAGS"
-# export CPPFLAGS="-I/usr/local/opt/mysql/include $CPPFLAGS"
-# export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/mysql/lib/pkgconfig"
+export LDFLAGS="-L/opt/homebrew/opt/mysql/lib $LDFLAGS"
+export CPPFLAGS="-I/opt/homebrew/opt/mysql/include $CPPFLAGS"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/mysql/lib/pkgconfig:$PKG_CONFIG_PATH"
