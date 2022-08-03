79180689b6fc982cc4b7ecf2dbe9c27dc7d9ab59
Thomas von Deyen <thomas@vondeyen.com>
2022-08-03 15:59:39 +0200

    Update to postgres 14

--- postgresql/path.zsh
+++ postgresql/path.zsh
@@ -1,4 +1,4 @@
-export PATH="/Applications/Postgres.app/Contents/Versions/12/bin:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/14/bin:$PATH"
 # Installed libpq via homebrew
 export LDFLAGS="-L/opt/homebrew/opt/libpq/lib $LDFLAGS"
 export CPPFLAGS="-I/opt/homebrew/opt/libpq/include $CPPFLAGS"
