202e7d6971d974c61378a83e587a633658185650
Thomas von Deyen <thomas@vondeyen.com>
2025-08-18 13:34:16 +0200

    Update path to latest libsass version

--- sass/path.zsh
+++ sass/path.zsh
@@ -1,2 +1,2 @@
-export LDFLAGS="-L/opt/homebrew/Cellar/libsass/3.6.5/lib $LDFLAGS"
-export CPPFLAGS="-I/opt/homebrew/Cellar/libsass/3.6.5/include $CPPFLAGS"
+export LDFLAGS="-L/opt/homebrew/Cellar/libsass/3.6.6/lib $LDFLAGS"
+export CPPFLAGS="-I/opt/homebrew/Cellar/libsass/3.6.6/include $CPPFLAGS"
