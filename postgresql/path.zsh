6b5fe02e506136a6bd895e8821692d577a8714a4
Thomas von Deyen <thomas@vondeyen.com>
2025-11-13 08:20:59 +0100

    default to postgres 14 for local install

--- postgresql/path.zsh
+++ postgresql/path.zsh
@@ -1,4 +1,3 @@
 export PATH="/Applications/Postgres.app/Contents/Versions/14/bin:$PATH"
-# Installed libpq via homebrew
-export LDFLAGS="-L/opt/homebrew/opt/libpq/lib $LDFLAGS"
-export CPPFLAGS="-I/opt/homebrew/opt/libpq/include $CPPFLAGS"
+export LDFLAGS="-L/Applications/Postgres.app/Contents/Versions/14/lib $LDFLAGS"
+export CPPFLAGS="-I/Applications/Postgres.app/Contents/Versions/14/include $CPPFLAGS"
