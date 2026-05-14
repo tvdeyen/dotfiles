be6b3816c0f63c267e15c3f59b07f05fdbabc1fd
Thomas von Deyen <thomas@vondeyen.com>
2026-05-14 09:39:56 +0200

    use latest for postgres cli tools

--- postgresql/path.zsh
+++ postgresql/path.zsh
@@ -1,3 +1,3 @@
-export PATH="/Applications/Postgres.app/Contents/Versions/14/bin:$PATH"
-export LDFLAGS="-L/Applications/Postgres.app/Contents/Versions/14/lib $LDFLAGS"
-export CPPFLAGS="-I/Applications/Postgres.app/Contents/Versions/14/include $CPPFLAGS"
+export PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
+export LDFLAGS="-L/Applications/Postgres.app/Contents/Versions/latest/lib $LDFLAGS"
+export CPPFLAGS="-I/Applications/Postgres.app/Contents/Versions/latest/include $CPPFLAGS"
