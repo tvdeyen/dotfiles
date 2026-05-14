3d40b796f7a73f5f1ab16ccd4e36e2f14cb3b445
Thomas von Deyen <thomas@vondeyen.com>
2026-05-14 09:39:37 +0200

    Go back to default open ssl

--- ruby/openssl.zsh
+++ ruby/openssl.zsh
@@ -1,4 +1,3 @@
-export LDFLAGS="-L/opt/homebrew/opt/openssl@3.5/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/openssl@3.5/include"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3.5/lib/pkgconfig"
-echo "⚠️ ACHTUNG ☢️\nUsing outdated OpenSSL 3.5.4!!\nTry to update to OpenSSL 3.6.1 after this Ruby bug (*) has been fixed and delete $(basename $0)\n\n(*) https://github.com/ruby/openssl/issues/949"
+export LDFLAGS="-L/opt/homebrew/opt/openssl/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/openssl/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl/lib/pkgconfig"
